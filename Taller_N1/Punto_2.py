4c354ea7448aba700d2d7f9d93d56c74cff7b143
juanpkz <125420608+juanpkz@users.noreply.github.com>
2023-03-07 13:38:31 -0500

    Delete Taller_N1 directory

--- Taller_N1/Punto_2.py
+++
@@ -1,6 +0,0 @@
-print('Indice de masa corporal')
-a=eval(input('Ingrese su estatura en cm : '))
-b=eval(input('Ingrese su peso en kg : '))
-m=a/100
-IMC=b/(m)**2
-print('su IMC es : ',round(IMC,4))
--- Taller_N1/Punto_3.py
+++
@@ -1,6 +0,0 @@
-print('Calculo del IVA')
-a=eval(input('Ingrese el valor del producto : '))
-bruto=a/1.19
-IVA=bruto*0.19
-print('El valor del IVA es : ',round(IVA,2))
-print('El valor bruto es : ',round(bruto,2))
